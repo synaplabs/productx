dc0791ca46a5223b097f52e80f0eb10918342e91
AryaStark13 <arihant.sheth0802@gmail.com>
2023-07-18 15:49:39 +0530

    updated streamlit website

--- streamlit_web/generate_chat_prd_gpt.py
+++ streamlit_web/generate_chat_prd_gpt.py
@@ -8,9 +8,6 @@ import wandb
 from wandb.integration.langchain import WandbTracer
 import streamlit as st
 
-
-from IPython.display import display, Markdown
-
 string_template = """\
 You are a smart product manager who answers in a concise way. You have to help the user create a Product Requirement Document (PRD) based on the questions the user asks you. The user will ask you specific questions about each topic they want to be included in the PRD. 
 
@@ -43,8 +40,24 @@ chain = LLMChain(
     verbose=False
 )
 
-total_tokens = 0
-total_cost = 0.0
+prompts_list = [
+    """Product Overview:
+Define the Purpose and Scope of this product. It should include how different groups of users across ages, genders, and geographies can use this product. Include an overview of the product. Why should one use this product? Define the target audience and stakeholders in detail. Also, include the rationale behind having the particular group as the target audience. Explain the gap it is trying to fill as well - how it is different from and better than other similar products?""",
+    """Product Objectives:
+First, analyze whether the product objectives align with the company objectives. Think aloud. Explain your reasoning. Also, talk about why and how the business models of the product and company match. What company goals can the product help achieve - be it attracting customers, generating profits, or promoting the goodwill of the company? Also, explain how it would do this.""",
+    """Market Research:
+First, list out current and potential competitors. Current competitors should include already established businesses/products. Potential competitors should include products and businesses that aren’t yet popular or are still under development/ beta version. Also include major or minor differences between our product and the competitor products you have identified. Analyze how aspects of our product or competitor products are better for that particular aspect. How do the target customers different? Does our product better cater to current trends and expectations of the users? How? What should the product include to meet those trends and expectations.""",
+    """Competitive Analysis Table:
+Use all the above competitors to create a competitive analysis of these applications in a tabular form using the following points - user base, user region, different features supported, and pricing tiers. Don't limit yourself to these categories and think of other categories yourself. Return the output in a well-structured Markdown table""",
+    """Feature Requirements:
+What are some of the important features that should be implemented? Follow the MoSCoW format (Must have, Should have, Could have, Won’t have, along with why). How are we going to collect user inputs and use user data that we collect to make the product better and add other features?""",
+    """Acceptance Criteria:
+Define the quality of completeness required to be able to get to the MVP stage of this product.""",
+    """Success Metrics:
+How do we define success in this product? What are the KPIs to look out for? How are they measured? Why do those KPIs matter? How are we going to use these KPIs to make the product better?""",
+    """Technical Feasibilities:
+Outline the technical roadmap for this product. What mobile devices should this application be available for? What is a scalable and reliable tech stack which can be used for the frontend and the backend for this application?""",
+]
 
 
 def generate_chat_prd_gpt(product_name, product_desc, wandb_name):
@@ -61,87 +74,31 @@ def generate_chat_prd_gpt(product_name, product_desc, wandb_name):
         name=wandb_name,
     )
 
+    output = ""
+
     with get_openai_callback() as callback:
 
         initial_output = chain.predict(
-            input="""\
+            input=f"""\
 I want to add the following new feature:
-Instagram Threads.
+{product_name}.
 
-Feature description: A new app by Instagram that allows users to post tweet-like content.
+Feature description: {product_desc}.
 
 DO NOT START WRITING. WAIT FOR THE HUMAN TO WRITE "Start generating the PRD" BEFORE YOU START WRITING.
-""",
-            callbacks=[WandbTracer()]
+    """
         )
 
-        output = chain.predict(
-            input="""\
-Define the Purpose and Scope of this product. It should include how different groups of users across ages, genders, and geographies can use this product. Include an overview of the product. Why should one use this product? Define the target audience and stakeholders in detail. Also, include the rationale behind having the particular group as the target audience. Explain the gap it is trying to fill as well - how it is different from and better than other similar products?
-Start writing the PRD now. Each question that follows will be a section in the PRD. Please write the PRD in markdown format. Only include the question heading in the PRD, but do not include any other text from the question. End your response with \n
-""",
-            callbacks=[WandbTracer()]
-        )
+        for i, prompt in enumerate(prompts_list):
 
-        output += "\n\n"
+            output += chain.predict(
+                input=prompt,
+                callbacks=[WandbTracer()]
+            )
 
-        output += chain.predict(
-            input="""\
-First, analyze whether the product objectives align with the company objectives. Think aloud. Explain your reasoning. Also, talk about why and how the business models of the product and company match. What company goals can the product help achieve - be it attracting customers, generating profits, or promoting the goodwill of the company? Also, explain how it would do this.""",
-            callbacks=[WandbTracer()]
-        )
+            output += "\n\n"
 
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-First, list out current and potential competitors. Current competitors should include already established businesses/products. Potential competitors should include products and businesses that aren’t yet popular or are still under development/ beta version. Also include major or minor differences between our product and the competitor products you have identified. Analyze how aspects of our product or competitor products are better for that particular aspect. How do the target customers different? Does our product better cater to current trends and expectations of the users? How? What should the product include to meet those trends and expectations.""",
-            callbacks=[WandbTracer()]
-        )
-
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-Use all the above competitors to create a competitive analysis of these applications in a tabular form using the following points - user base, user region, different features supported, and pricing tiers. Don't limit yourself to these categories and think of other categories yourself. Return the output in a well-structured Markdown table""",
-            callbacks=[WandbTracer()]
-        )
-
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-What are some of the important features that should be implemented? Follow the MoSCoW format (Must have, Should have, Could have, Won’t have, along with why). How are we going to collect user inputs and use user data that we collect to make the product better and add other features?""",
-            callbacks=[WandbTracer()]
-        )
-
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-Define the quality of completeness required to be able to get to the MVP stage of this product.""",
-            callbacks=[WandbTracer()]
-        )
-
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-How do we define success in this product? What are the KPIs to look out for? How are they measured? Why do those KPIs matter? How are we going to use these KPIs to make the product better?""",
-            callbacks=[WandbTracer()]
-        )
-
-        output += "\n\n"
-
-        output += chain.predict(
-            input="""\
-Outline the technical roadmap for this product. What mobile devices should this application be available for? What is a scalable and reliable tech stack which can be used for the frontend and the backend for this application?""",
-            callbacks=[WandbTracer()]
-        )
-
-        print(callback)
-
-        wandb.log({f"{product_name}_prd_gpt-4": output})
+        wandb.log({"prd": output})
         wandb.finish()
 
-        return output, callback
+    return output, callback
--- streamlit_web/main.py
+++ streamlit_web/main.py
@@ -8,23 +8,13 @@ from generate_prd_v3_palm import generate_prd_v3_palm
 from generate_chat_prd_gpt import generate_chat_prd_gpt
 from generate_chat_prd_vertexai import generate_chat_prd_vertexai
 
+
 def get_prd(new_feature, new_feature_desc, prd_version):
     wandb_name = f"{new_feature}_prd_{prd_version}"
     start_time = time.time()
-    if prd_version == "Version 1":
-        output = generate_prd_v1(new_feature, new_feature_desc, wandb_name)
+    callback = None
 
-    elif prd_version == "Version 2":
-        output = generate_prd_v2(new_feature, new_feature_desc, wandb_name)
-
-    elif prd_version == "Version 3 (GPT-3.5-Turbo)":
-        output = generate_prd_v3(new_feature, new_feature_desc, wandb_name)
-
-    elif prd_version == "Version 3 (Palm)":
-        output = generate_prd_v3_palm(
-            new_feature, new_feature_desc, wandb_name)
-
-    elif prd_version == "Chat PRD (GPT-4)":
+    if prd_version == "Chat PRD (GPT-4)":
         output, callback = generate_chat_prd_gpt(
             new_feature, new_feature_desc, wandb_name)
 
@@ -34,7 +24,7 @@ def get_prd(new_feature, new_feature_desc, prd_version):
 
     end_time = time.time()
     total_time = str(int(end_time - start_time))
-    return output, total_time
+    return output, total_time, callback
 
 
 def main():
@@ -44,16 +34,30 @@ def main():
         "Feature Description:", value="Enables users to capture photos from both front and back cameras simultaneously.")
 
     # Create a button and check if both text input fields are not empty before enabling it
-    prd_version = st.radio("Select PRD Version:",
-                           ("Version 1", "Version 2", "Version 3 (GPT-3.5-Turbo)", "Version 3 (Palm)", "Chat PRD (GPT-4)", "Chat PRD (Vertex AI)"))
+    prd_version = st.radio(
+        "Select PRD Version:",
+        ("Chat PRD (GPT-4)", "Chat PRD (Vertex AI)")
+    )
+
     if st.button("Get PRD", disabled=not (feature_name_input and feature_description_input)):
+        for key in st.session_state.keys():
+            del st.session_state[key]
+
         with st.spinner(text="Generating PRD..."):
             if 'output' not in st.session_state:
-                st.session_state.output, st.session_state.total_time = get_prd(
+                st.session_state.output, st.session_state.total_time, st.session_state.callback = get_prd(
                     new_feature=feature_name_input, new_feature_desc=feature_description_input, prd_version=prd_version)
                 st.session_state.edited_output = st.session_state.output
 
     if 'output' in st.session_state and 'total_time' in st.session_state and 'edited_output' in st.session_state:
+        if st.session_state.callback is not None:
+            n_requests = st.session_state.callback.successful_requests
+            total_cost = st.session_state.callback.total_cost
+            total_tokens = st.session_state.callback.total_tokens
+            st.write(f"Total requests: {n_requests}")
+            st.write(f"Total cost: ${total_cost:.2f}")
+            st.write(f"Total tokens: {total_tokens:,}")
+
         st.write(f"Time taken: {st.session_state.total_time} seconds")
 
         st.download_button(label="Download PRD", data=st.session_state.edited_output,
@@ -68,7 +72,7 @@ def main():
             with col1:
                 st.text("Markdown Editor")
                 st.session_state.edited_output = st.text_area(
-                    label="Markdown Editor", value=st.session_state.output, height=500, label_visibility="hidden")
+                    label="Markdown Editor", value=st.session_state.output, height=800, label_visibility="hidden")
             with col2:
                 st.text("Live Preview")
                 st.markdown(st.session_state.edited_output,
@@ -76,7 +80,8 @@ def main():
 
             st.session_state.output = st.session_state.edited_output
 
-        st.markdown(st.session_state.edited_output, help="Generated PRD")
+        else:
+            st.markdown(st.session_state.edited_output, help="Generated PRD")
 
 
 if __name__ == "__main__":
