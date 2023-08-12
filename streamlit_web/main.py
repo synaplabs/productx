637ba713594aaafc5db8e96749353674d10ed5fb
AryaStark13 <arihant.sheth0802@gmail.com>
2023-08-12 10:54:12 +0530

    updated streamlit website

--- streamlit_web/main.py
+++ streamlit_web/main.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import time
-from generate_chat_prd_gpt import generate_chat_prd_gpt
-from generate_chat_prd_vertexai import generate_chat_prd_vertexai
+from generate_web_chat_prd_gpt import main as generate_web_chat_prd_gpt
 
 
 def get_prd(new_feature, new_feature_desc, prd_version):
     wandb_name = f"{new_feature}_prd_{prd_version}"
     start_time = time.time()
-    callback = None
 
-    if prd_version == "Chat PRD (GPT-4)":
-        output, callback = generate_chat_prd_gpt(
-            new_feature, new_feature_desc, wandb_name)
+    if prd_version == "Web Chat PRD (GPT-4)":
+        output, cost = generate_web_chat_prd_gpt(product_name=new_feature,
+                                                 product_description=new_feature_desc)
 
-    elif prd_version == "Chat PRD (Vertex AI)":
-        output = generate_chat_prd_vertexai(
-            new_feature, new_feature_desc, wandb_name)
+    # elif prd_version == "Chat PRD (Vertex AI)":
+    #     output = generate_chat_prd_vertexai(
+    #         new_feature, new_feature_desc, wandb_name)
 
     end_time = time.time()
     total_time = str(int(end_time - start_time))
-    return output, total_time, callback
+    return output, total_time, cost
 
 
 def main():
@@ -29,10 +27,10 @@ def main():
         "Feature Description:", value="Enables users to capture photos from both front and back cameras simultaneously.")
 
     # Create a button and check if both text input fields are not empty before enabling it
-    prd_version = st.radio(
-        "Select PRD Version:",
-        ("Chat PRD (GPT-4)", "Chat PRD (Vertex AI)")
-    )
+    # prd_version = st.radio(
+    #     "Select PRD Version:", "Web Chat PRD (GPT-4)"
+    # )
+    prd_version = "Web Chat PRD (GPT-4)"
 
     if st.button("Get PRD", disabled=not (feature_name_input and feature_description_input)):
         for key in st.session_state.keys():
@@ -40,18 +38,19 @@ def main():
 
         with st.spinner(text="Generating PRD..."):
             if 'output' not in st.session_state:
-                st.session_state.output, st.session_state.total_time, st.session_state.callback = get_prd(
+                st.session_state.output, st.session_state.total_time, st.session_state.cost = get_prd(
                     new_feature=feature_name_input, new_feature_desc=feature_description_input, prd_version=prd_version)
                 st.session_state.edited_output = st.session_state.output
 
     if 'output' in st.session_state and 'total_time' in st.session_state and 'edited_output' in st.session_state:
-        if st.session_state.callback is not None:
-            n_requests = st.session_state.callback.successful_requests
-            total_cost = st.session_state.callback.total_cost
-            total_tokens = st.session_state.callback.total_tokens
-            st.write(f"Total requests: {n_requests}")
+        if st.session_state.cost is not None:
+            total_cost = st.session_state.cost["prd"]["cost"] + st.session_state.cost["db"]["cost"]
+            prompt_tokens = st.session_state.cost["prd"]["prompt_tokens"] + st.session_state.cost["db"]["prompt_tokens"]
+            completion_tokens = st.session_state.cost["prd"]["completion_tokens"] + st.session_state.cost["db"]["completion_tokens"]
             st.write(f"Total cost: ${total_cost:.2f}")
-            st.write(f"Total tokens: {total_tokens:,}")
+            st.write(f"Prompt Tokens: {prompt_tokens:,}")
+            st.write(f"Completion tokens: {completion_tokens:,}")
+            st.write(f"Total tokens: {prompt_tokens+completion_tokens:,}")
 
         st.write(f"Time taken: {st.session_state.total_time} seconds")
 
